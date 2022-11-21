8a2ab169e2756718c592adaf7fc79eaf425f6489
Adam Wright <adamtwright7@gmail.com>
2022-11-20 22:54:02 -0500

    Completed the test exercise and the Microsoft reading

---
+++ week2/day1/test_exercise.py
@@ -0,0 +1,33 @@
+# function which takes a string, like a user input, and converts it into its appropriate boolean value. 
+def str_to_bool(value):
+    try:
+        value = value.lower()
+    except AttributeError:
+        raise AttributeError(f"{value} must be of type string")
+    true_values = ['y','yes']
+    false_values = ['no', 'n']
+
+    if value in true_values:
+        return True
+    if value in false_values:
+        return False
+
+# tests for the above function 
+import unittest
+
+class TestStrToBool(unittest.TestCase):
+
+    def test_y_is_true(self):
+        result = str_to_bool('y')
+        self.assertTrue(result)
+
+    def test_yes_is_true(self):
+        result = str_to_bool('Yes')
+        self.assertTrue(result)
+    def test_invalid_input(self):
+        with self.assertRaises(AttributeError):
+            str_to_bool(1)
+
+if __name__ == '__main__':
+    unittest.main()
+
